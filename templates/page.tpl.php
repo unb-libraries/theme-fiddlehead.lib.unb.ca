9b7772e8ff7dcaf076c68d10c9f171ff3182d41c
Brian Cassidy <brian.cassidy@gmail.com>
2016-03-29 16:24:15 -0300

    mobile menu styles/display

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -70,7 +70,7 @@
         <div id="nav-search" class="nav-search"> <?php print render($page['search_box']); ?> </div>
         <?php endif; ?>
         <?php if ($main_menu): ?>
-        <button aria-label="Navigation menu" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse"> <span class="hide">Navigation menu</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <button aria-label="Navigation menu" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse"> <span class="hide">Menu</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         <?php if ($primary_nav || !empty($page['navigation'])): ?>
         <div class="nav-collapse collapse">
           <nav id="main-nav" role="navigation">
